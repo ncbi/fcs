ee47a5bb2c5c328479878ec3fa636c809026dc95
Deacon Sweeney <sweeneydj@nih.gov>
2022-07-18 16:34:10 -0400

    update dockerhub image

--- dist/run_fcsgx.py
+++ dist/run_fcsgx.py
@@ -12,7 +12,7 @@ import hashlib
 
 CONTAINER = "run_gx"
 DEFAULT_CONTAINER_DB = "/app/db/gxdb"
-DEFAULT_DOCKER_IMAGE = "ncbi/cgr-fcs-genome:beta"
+DEFAULT_DOCKER_IMAGE = "ncbi/fcs-genome:0.2.1"
 DEFAULT_SINGULARITY_IMAGE = "gx-develop-latest.sif"
 # FILE_MANIFEST = "sing-image.manifest"
 SINGULARITY_FTP_SITE = "https://ftp.ncbi.nlm.nih.gov/genomes/TOOLS/FCS/releases/beta"
